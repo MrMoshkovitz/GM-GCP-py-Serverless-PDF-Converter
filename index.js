e0cdc1c907a351b403f720ab24b865453093c7da
MrMoshkovitz <gal.moshko@gmail.com>
2021-03-07 06:56:38 +0200

    PDF-Converter-Stage-1-Done

--- index.js
+++ index.js
@@ -1,31 +1,59 @@
+const { promisify } = require("util");
+const { Storage } = require("@google-cloud/storage");
+const exec = promisify(require("child_process").exec);
+const storage = new Storage();
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
-const bodyParser = require("body-parser")
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+
 const port = process.env.PORT || 8080;
-
 app.listen(port, () => {
-    console.log("App Is Listening on Port: ",port);
-})
+	console.log("Listening on port", port);
+});
 
-app.get("/", (req,res) => {
-    res.set("Content-Type", "text/plain");
-    res.send({"message":"Success"})
-})
+app.post("/", async (req, res) => {
+	try {
+		const file = decodeBase64Json(req.body.message.data);
+		await downloadFile(file.bucket, file.name);
+		const pdfFileName = await convertFile(file.name);
+		await uploadFile(process.env.PDF_BUCKET, pdfFileName);
+		await deleteFile(file.bucket, file.name);
+	} catch (ex) {
+		console.log(`Error: ${ex}`);
+	}
+	res.set("Content-Type", "text/plain");
+	res.send("\n\nOK\n\n");
+});
 
-app.post("/", async(req,res) => {
-    console.log("Post OK")
-    try {
-        const file = B64Decoder(req.body.message.data);
-        console.log(`File: ${JSON.stringify(file)}`);
-    } catch(ex) {
-        console.log(ex);
-    }
-    res.set("Content-Type", "text/plain");
-    res.send("\n\nPost OK\n\n")
-})
+function decodeBase64Json(data) {
+	return JSON.parse(Buffer.from(data, "base64").toString());
+}
 
-const B64Decoder = (data) => {
-    return JSON.parse(Buffer.from(data, "base64").toString());
+async function downloadFile(bucketName, fileName) {
+	const options = { destination: `/tmp/${fileName}` };
+	await storage.bucket(bucketName).file(fileName).download(options);
+}
+
+async function convertFile(fileName) {
+	const cmd =
+		"libreoffice --headless --convert-to pdf --outdir /tmp " +
+		`"/tmp/${fileName}"`;
+	console.log(cmd);
+	const { stdout, stderr } = await exec(cmd);
+	if (stderr) {
+		throw stderr;
+	}
+	console.log(stdout);
+	pdfFileName = fileName.replace(/\.\w+$/, ".pdf");
+	return pdfFileName;
+}
+
+async function deleteFile(bucketName, fileName) {
+	await storage.bucket(bucketName).file(fileName).delete();
+}
+
+async function uploadFile(bucketName, fileName) {
+	await storage.bucket(bucketName).upload(`/tmp/${fileName}`);
 }
