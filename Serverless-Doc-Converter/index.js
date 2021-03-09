1adabe892fc57b7c23ddafec54d9e2b4b2d992d7
MrMoshkovitz <gal.moshko@gmail.com>
2021-03-09 21:33:08 +0200

    fixing indentaion error

--- Serverless-Doc-Converter/index.js
+++ Serverless-Doc-Converter/index.js
@@ -21,7 +21,7 @@ app.listen(port, () => {
 app.post("/", async (req, res) => {
 	try {
 		const file = decodeBase64Json(req.body.message.data);
-        let fileName = file.name
+		let fileName = file.name;
 		await downloadFile(file.bucket, fileName);
 		const docxfileName = await convertFile(fileName);
 		await uploadFile(process.env.DOCX_BUCKET, docxfileName);
@@ -37,73 +37,71 @@ const decodeBase64Json = (data) => {
 };
 
 const downloadFile = async (bucketName, fileName) => {
-    console.log(`File Name1: ${fileName}`);
+	console.log(`File Name1: ${fileName}`);
 	const options = { destination: `/tmp/${fileName}` };
 	await storage.bucket(bucketName).file(fileName).download(options);
 };
 
 convertFile = async (fileName) => {
-    console.log(`File Name2: ${fileName}`);
+	console.log(`File Name2: ${fileName}`);
 	// Adding authentication headers
 	const headers = {
 		"X-ApplicationID": process.env.AppID,
 		"X-SecretKey": process.env.AppKey,
 	};
 
-    try {
-        let conversionRequest = request.post(
-            {
-                url: "https://api2.docconversionapi.com/jobs/create",
-                formData: formData,
-                rejectUnauthorized: false,
-                headers: headers,
-                encoding: "binary",
-            },
-            (err, response) => {
-                let responseParsed = null;
+	try {
+		let conversionRequest = request.post(
+			{
+				url: "https://api2.docconversionapi.com/jobs/create",
+				formData: formData,
+				rejectUnauthorized: false,
+				headers: headers,
+				encoding: "binary",
+			},
+			(err, response) => {
+				let responseParsed = null;
 
-                if (response.statusCode == 200) {
-                // Parsing response JSON object and getting fileDownloadUrl value
-                    responseParsed = JSON.parse(response.body)
-                    console.log("URL To Download: " , responseParsed.fileDownloadURL);
+				if (response.statusCode == 200) {
+					// Parsing response JSON object and getting fileDownloadUrl value
+					responseParsed = JSON.parse(response.body);
+					console.log("URL To Download: ", responseParsed.fileDownloadURL);
 
-                // Downloading the file and writing it to the local folder
-                const resultFile = fs.createWriteStream(`/tmp/${fileName}`)
-                https.get(responseParsed.fileDownloadURL, (res) => {
-                    res.pipe(resultFile)
-                })
-                console.log("Result File");
-                console.log(resultFile);
-                }
-                
-                if (err) {
-                    throw err
-                }
-        }
+					// Downloading the file and writing it to the local folder
+					const resultFile = fs.createWriteStream(`/tmp/${fileName}`);
+					https.get(responseParsed.fileDownloadURL, (res) => {
+						res.pipe(resultFile);
+					});
+					console.log("Result File");
+					console.log(resultFile);
+				}
 
-    )
-    // Adding all parameters to multipart form
-    let form = conversionRequest.form();
-    form.append("inputFile", fs.readFileSync(`/tmp/${fileName}`))
-    form.append("conversionParameters", "{}");
-    form.append("async", "false");
-    form.append("outputFormat", "docx");
-
-    } catch (ex) {
-        console.log(`Error: ${ex}`);
-    }
+				if (err) {
+					throw err;
+				}
+			}
+		);
+		// Adding all parameters to multipart form
+		let form = conversionRequest.form();
+		form.append("inputFile", fs.readFileSync(`/tmp/${fileName}`));
+		form.append("conversionParameters", "{}");
+		form.append("async", "false");
+		form.append("outputFormat", "docx");
+	} catch (ex) {
+		console.log(`Error: ${ex}`);
+	}
 };
 
 uploadFile = async (bucketName, fileName) => {
-    await storage.bucket(bucketName).upload(`/tmp/${fileName}`).then(console.log("File Uploaded Successfuly"))
-}
+	await storage
+		.bucket(bucketName)
+		.upload(`/tmp/${fileName}`)
+		.then(console.log("File Uploaded Successfuly"));
+};
 
 // Adding all parameters to multipart form
 var form = conversionRequest.form();
-form.append(
-	"inputFile",
-	`/tmp/${fileName}`
-);
+form.append("inputFile", `/tmp/${fileName}`);
 form.append("conversionParameters", "{}");
 form.append("async", "false");
 form.append("outputFormat", "docx");
